90fcde6aca483c8173859cbbe93fb1264f2b5d30
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-07-22 15:13:14 +0530

    feat: deploy Spring Boot application to AWS EC2 using Docker

---
+++ AWS-Deployement-CICD/Hello-app/src/main/java/com/sagar/Hello_app/HelloController.java
@@ -0,0 +1,13 @@
+package com.sagar.Hello_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello GitHub Actions from CI/CD Pipeline";
+    }
+}
